69b482b114ab28c6852f3ae8ee833c0dbaa37082
Long Dinh <tronglongcntt@gmail.com>
2023-05-06 18:47:06 +0700

    add constructor which accept cause throwable

--- netz-http/src/main/java/com/github/longdt/netz/http/exception/NoStackTraceException.java
+++ netz-http/src/main/java/com/github/longdt/netz/http/exception/NoStackTraceException.java
@@ -4,4 +4,8 @@ public class NoStackTraceException extends RuntimeException {
   public NoStackTraceException(String message) {
     super(message, null, false, false);
   }
+
+  public NoStackTraceException(String message, Throwable cause) {
+    super(message, cause, false, false);
+  }
 }
